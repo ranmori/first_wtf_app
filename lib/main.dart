6af920404c82b50a11f38692825b101e1e864de7
anto <antobuilds@gmail.com>
2025-10-21 12:49:32 +0100

    updTED

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:first_wtf_app/pages/login_page.dart';
 import 'package:first_wtf_app/pages/onboarding_page.dart';
 import 'package:first_wtf_app/pages/signup_page.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(const MyApp());
@@ -18,14 +19,16 @@ class MyApp extends StatelessWidget {
       title: 'Save A Life',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
+        textTheme: GoogleFonts.robotoTextTheme()
       ),
       debugShowCheckedModeBanner: false, 
       routes: {
-        "/": (context)=> OnboardingPage(),
+        "/o": (context)=> OnboardingPage(),
         "/home": (context)=> BottomNavigation(),
         "/login": (context)=> LoginPage(),
         "/signup": (context)=> SignupPage()
       },
+      initialRoute: "/home",
       // home: BottomNavigation()
     );
   }
