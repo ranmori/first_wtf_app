35f769f9412bbf78ad6db4b94d32b789ec070eb0
Naema <94893024+ranmori@users.noreply.github.com>
2025-12-14 20:04:42 +0300

    Refactor PasswordTextfield for improved readability

--- lib/widgets/password_textfield.dart
+++ lib/widgets/password_textfield.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
 
 class PasswordTextfield extends StatefulWidget {
-  const PasswordTextfield({super.key, this.textEditingController, this.label = "Password"});
+  const PasswordTextfield({
+    super.key,
+    this.textEditingController,
+    this.label = "Password",
+  });
+
   final TextEditingController? textEditingController;
   final String label;
 
@@ -16,16 +21,28 @@ class _PasswordTextfieldState extends State<PasswordTextfield> {
   Widget build(BuildContext context) {
     return TextField(
       controller: widget.textEditingController,
+      style: const TextStyle(
+        color: Colors.white, // typing text color
+      ),
+      cursorColor: Colors.white, //  cursor color
       decoration: InputDecoration(
-        border: OutlineInputBorder(borderRadius: BorderRadius.circular(16)),
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(16),
+        ),
         labelText: widget.label,
+        labelStyle: const TextStyle(
+          color: Colors.white70,
+        ),
         suffixIcon: InkWell(
           onTap: () {
             setState(() {
               isVisible = !isVisible;
             });
           },
-          child: Icon(isVisible ? Icons.visibility : Icons.visibility_off),
+          child: Icon(
+            isVisible ? Icons.visibility : Icons.visibility_off,
+            color: Colors.white, // icon color
+          ),
         ),
       ),
       obscureText: !isVisible,
